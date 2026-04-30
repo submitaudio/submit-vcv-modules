80b5ad2b2b4725221d70ce4187a6aed7c935894b
Studio67 <studio67@mbpvanstudio67.home>
2026-04-30 21:55:04 +0200

    Fix Shape panel, add clip LEDs Chain, fix module order, copyright headers

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -4,23 +4,15 @@
 
 #include "plugin.hpp"
 
-
 Plugin* pluginInstance;
-
 
 void init(Plugin* p) {
 	p->addModel(modelDrift);
 	p->addModel(modelChrono);
+	p->addModel(modelImpact);
 	p->addModel(modelChain);
 	p->addModel(modelSqueeze);
 	p->addModel(modelShape);
 	p->addModel(modelMaster);
 	pluginInstance = p;
-
-	// Add modules here
-	// p->addModel(modelMyModule);
-	p->addModel(modelImpact);
-
-	// Any other plugin initialization may go here.
-	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
 }
